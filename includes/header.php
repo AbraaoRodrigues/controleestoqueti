637a423eef0e3939e5fa14bf7c909c80aa675cbd
AbraaoRodrigues <abraaoisrael@gmail.com>
2025-07-17 10:06:43 -0300

    V0.1 alteração menu superior, criação de configuração de perfil, avatar, e logs

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,24 @@
-<!DOCTYPE html><html><head><meta charset='utf-8'><link rel='stylesheet' href='assets/css/style.css'><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet'></head><body>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <link rel='stylesheet' href='/controle_estoque_ti/assets/css/style.css'>
+  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet'>
+</head>
+<body>
+<nav class='navbar navbar-expand-lg navbar-light bg-light px-3'>
+  <a class='navbar-brand' href='/controle_estoque_ti/dashboard.php'>Estoque TI</a>
+  <div class='ms-auto'>
+    <div class='dropdown'>
+      <a class='d-flex align-items-center text-decoration-none dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+        <img src='/controle_estoque_ti/assets/img/avatar.png' class='avatar me-2'>
+        <span>Usuário</span>
+      </a>
+      <ul class='dropdown-menu dropdown-menu-end'>
+        <li><a class='dropdown-item' href='/controle_estoque_ti/usuarios/perfil.php'>Meu Perfil</a></li>
+        <li><hr class='dropdown-divider'></li>
+        <li><a class='dropdown-item' href='/controle_estoque_ti/logout.php'>Sair</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
